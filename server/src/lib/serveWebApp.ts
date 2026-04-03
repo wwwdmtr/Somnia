f3c74626ac2dbf39df1af4e4cf718644d4fa9778
wwwdmtr <vodolazskiy389@gmail.com>
2026-04-03 21:27:20 +0300

    Added dockerfile image, github deploy workflows and nginx config

--- server/src/lib/serveWebApp.ts
+++ server/src/lib/serveWebApp.ts
@@ -1,47 +1,49 @@
-import { promises as fs } from "fs";
-import path from "path";
+// In case you want to serve the web app from the same server, you can set up the static file serving here.
 
-import express, { type Express } from "express";
+// import { promises as fs } from "fs";
+// import path from "path";
 
-import { env } from "./env";
-import { logger } from "./logger";
+// import express, { type Express } from "express";
 
-const checkFileExists = async (filePath: string) => {
-  return await fs
-    .access(filePath, fs.constants.F_OK)
-    .then(() => true)
-    .catch(() => false);
-};
+// import { env } from "./env";
+// import { logger } from "./logger";
 
-const findWebappDistDir = async (dir: string): Promise<string | null> => {
-  const maybeWebappDistDir = path.resolve(dir, "webapp/dist");
-  if (await checkFileExists(maybeWebappDistDir)) {
-    return maybeWebappDistDir;
-  }
-  if (dir === "/") {
-    return null;
-  }
-  return await findWebappDistDir(path.dirname(dir));
-};
+// const checkFileExists = async (filePath: string) => {
+//   return await fs
+//     .access(filePath, fs.constants.F_OK)
+//     .then(() => true)
+//     .catch(() => false);
+// };
 
-export const applyServeWebApp = async (expressApp: Express) => {
-  const webappDistDir = await findWebappDistDir(__dirname);
-  if (!webappDistDir) {
-    if (env.NODE_ENV === "production") {
-      throw new Error("Webapp dist dir not found");
-    } else {
-      logger.error("webapp-serve", "Webapp dist dir not found");
-      return;
-    }
-  }
+// const findWebappDistDir = async (dir: string): Promise<string | null> => {
+//   const maybeWebappDistDir = path.resolve(dir, "webapp/dist");
+//   if (await checkFileExists(maybeWebappDistDir)) {
+//     return maybeWebappDistDir;
+//   }
+//   if (dir === "/") {
+//     return null;
+//   }
+//   return await findWebappDistDir(path.dirname(dir));
+// };
 
-  const htmlSource = await fs.readFile(
-    path.resolve(webappDistDir, "index.html"),
-    "utf8",
-  );
+// export const applyServeWebApp = async (expressApp: Express) => {
+//   const webappDistDir = await findWebappDistDir(__dirname);
+//   if (!webappDistDir) {
+//     if (env.NODE_ENV === "production") {
+//       throw new Error("Webapp dist dir not found");
+//     } else {
+//       logger.error("webapp-serve", "Webapp dist dir not found");
+//       return;
+//     }
+//   }
 
-  expressApp.use(express.static(webappDistDir, { index: false }));
-  expressApp.get("/{*splat}", (_req, res) => {
-    res.send(htmlSource);
-  });
-};
+//   const htmlSource = await fs.readFile(
+//     path.resolve(webappDistDir, "index.html"),
+//     "utf8",
+//   );
+
+//   expressApp.use(express.static(webappDistDir, { index: false }));
+//   expressApp.get("/{*splat}", (_req, res) => {
+//     res.send(htmlSource);
+//   });
+// };
